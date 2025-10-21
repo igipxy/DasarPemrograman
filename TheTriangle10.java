fe5234915cbdf829de811e684b1180f916d095ba
iGipxy <gesangrah@gmail.com>
2025-10-21 10:24:37 +0700

    blablebo

--- TheTriangle10.java
+++ TheTriangle10.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class TheTriangle10 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numinput;
+        int i = 0;
+        String s = "";
+
+        System.out.println("Input some number:");
+        numinput = input.nextInt();
         
+        for (i = 1; i <= numinput; i++) {
+            s = s + "*";
+            System.out.println(s);
+            
+        }
+
+        input.close();
+        }
     }
 
-}
