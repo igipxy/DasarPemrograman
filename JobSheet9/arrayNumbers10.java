d266523820f2f019bee0017a9a5ebcfac434e2e2
Gesang <gesangrah@gmail.com>
2025-11-04 08:43:49 +0700

    NEW ASSIGNMENT

---
+++ JobSheet9/arrayNumbers10.java
@@ -0,0 +1,15 @@
+public class arrayNumbers10 {
+    public static void main(String[] args) {
+
+        int[] num = new int[4]; 
+        num[0] = 5;
+        num[1] = 15;
+        num[2] = 7;
+        num[3] = 20;
+
+        system.out.println(num[0]);
+        system.out.println(num[1]);
+        system.out.println(num[2]);
+        system.out.println(num[3]);
+    }
+}
