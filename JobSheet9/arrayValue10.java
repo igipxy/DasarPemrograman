5f2f51e9678f10b1958d3b99e3bacedda21d63d1
Gesang <gesangrah@gmail.com>
2025-11-04 09:25:34 +0700

    following the step

--- JobSheet9/arrayValue10.java
+++ JobSheet9/arrayValue10.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class arrayValue10 {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int[] finalScore = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < finalScore.length; i++) {
             System.out.print("Enter the final score " + i + ": ");
-            finalScore[i] = scanner.nextInt();
+            finalScore[i] = sc.nextInt();
         }
 
         for (int i = 0; i < 10; i++) {
             System.out.println("Final score " + i + " is " + finalScore[i]);
         }
 
-        scanner.close();
+        sc.close();
     }
 }
---
+++ JobSheet9/arrayValue10Answer.md
@@ -0,0 +1,7 @@
+Question !
+
+1. There is no diffrent but its like the alternative way to code
+
+2. The condition is for the loop to continues when  index is less than the array size
+
+3. 
