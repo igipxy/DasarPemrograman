625bc5982d293fd5a6dd3e813cd5a0b4bad1f19d
iGipxy <gesangrah@gmail.com>
2025-11-20 13:06:39 +0700

    Quizz 1.0

---
+++ QuizzWeek13/SistemKPU.java
@@ -0,0 +1,14 @@
+package QuizzWeek13;
+import java.util.Scanner;
+
+public class SistemKPU {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        /*make progam each region total vote for each candidate, and display table
+        count total vote for each candidate, and kpu want to know which place have the most vote for each candidate
+        and use array*/      
+    }
+
+}
